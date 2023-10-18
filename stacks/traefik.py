a7bc30dbf213980768abdae220dcb7c62dece31c
Daniel Tlach <daniel@tlach.cz>
2023-10-18 18:52:42 +0200

    Update traefik.py
    - sets a specific version of the Traefik image
    - fully configures ports
    - code cleanup

--- stacks/traefik.py
+++ stacks/traefik.py
@@ -6,14 +6,14 @@ def definition(provider: docker.Provider, network: docker.Network):
     try:
         # Pull a remote Docker image
         image = docker.RemoteImage("traefik_image",
-                                   name="traefik:latest")
+                                   name="traefik:v3.0")
 
         # Start a container
         container = docker.Container("traefik_container",
                                      name="traefik_container",
                                      image=image.name,
                                      opts=pulumi.ResourceOptions(provider=provider),
-                                     networks_advanced=[docker.ContainerNetworksAdvancedArgs(name=network.name)],
+                                     network_mode="host",
                                      volumes=[{
                                          "host_path": "/var/run/docker.sock",
                                          "container_path": "/var/run/docker.sock",
@@ -22,22 +22,23 @@ def definition(provider: docker.Provider, network: docker.Network):
                                      ports=[{
                                          "internal": 80,
                                          "external": 80,
+                                         "ip": "0.0.0.0",
+                                         "protocol": "tcp",
                                      }, {
                                          "internal": 443,
                                          "external": 443,
-                                     }, {
-                                         "internal": 8080,
-                                         "external": 8100,
+                                         "ip": "0.0.0.0",
+                                         "protocol": "tcp",
                                      }],
                                      envs=[
-                                         f"TRAEFIK_API_INSECURE=true",
-                                         f"TRAEFIK_API_DASHBOARD=true",
-                                         f"TRAEFIK_GLOBAL_CHECKNEWVERSION=true",
-                                         f"TRAEFIK_GLOBAL_SENDANONYMOUSUSAGE=true",
-                                         f"TRAEFIK_ENTRYPOINTS_HTTP_ADDRESS=:80",
-                                         f"TRAEFIK_ENTRYPOINTS_HTTPS_ADDRESS=:443",
-                                         f"TRAEFIK_PROVIDERS_DOCKER=true",
-                                         f"TRAEFIK_PROVIDERS_DOCKER_EXPOSEDBYDEFAULT=false",
+                                         "TRAEFIK_API_INSECURE=true",
+                                         "TRAEFIK_API_DASHBOARD=true",
+                                         "TRAEFIK_GLOBAL_CHECKNEWVERSION=true",
+                                         "TRAEFIK_GLOBAL_SENDANONYMOUSUSAGE=true",
+                                         "TRAEFIK_ENTRYPOINTS_HTTP_ADDRESS=:80",
+                                         "TRAEFIK_ENTRYPOINTS_HTTPS_ADDRESS=:443",
+                                         "TRAEFIK_PROVIDERS_DOCKER=true",
+                                         "TRAEFIK_PROVIDERS_DOCKER_EXPOSEDBYDEFAULT=false",
                                      ])
 
         # Export container information
